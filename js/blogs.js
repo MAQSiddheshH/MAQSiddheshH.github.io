c05e09bdb739b03b0b78ec1cdf1a1f0d755e216c
Siddhesh Deepak Hindalekar <v-sihin@microsoft.com>
2021-01-14 18:48:39 +0530

    Merge from PROD Website

--- js/blogs.js
+++ js/blogs.js
@@ -200,7 +200,7 @@ function renderCaseStudy() {
             });
             sImageLink = imgSource.getElementsByTagName('img')[0].getAttribute('src');
             sCaseStudyTitle = entry1.getElementsByTagName('title')[0].childNodes[0].nodeValue;
-            if (entry1) {                
+            if (entry1) {
                 var oCaseStudyEntry = "<div class='col-md-4 col-sm-4 nf-item spacing-grid " + aCategoryFilters.join(" ") + "'>"
                    + "<div class='blog-post'>"
                       + "<div class='post-media'>"
@@ -214,7 +214,7 @@ function renderCaseStudy() {
                        + "<div class='post-more-link pull-right'><a href='" + sAnchorCaseStudy + "'>Read More<i class='fa fa-long-arrow-right right'></i></a></div>"
                    + "</div>"
                    + "</div>"
-                oCaseStudyContainer.append(oCaseStudyEntry);              
+                oCaseStudyContainer.append(oCaseStudyEntry);
             }
         }
     }
